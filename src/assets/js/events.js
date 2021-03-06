a2df38046d2133f032e81e044e25bac2355b98d9
Pragnesh Katkar <pragneshpkatkar374@gmail.com>
2021-03-06 22:13:22 +0530

    Test
    Test

--- src/assets/js/events.js
+++ src/assets/js/events.js
@@ -2,71 +2,62 @@ import helpers from './helpers.js';
 
 window.addEventListener('load', ()=>{
     //When the chat icon is clicked
-    // document.querySelector('#toggle-chat-pane').addEventListener('click', (e)=>{
-    //     document.querySelector('#chat-pane').classList.toggle('chat-opened');
+    document.querySelector('#toggle-chat-pane').addEventListener('click', (e)=>{
+        document.querySelector('#chat-pane').classList.toggle('chat-opened');
 
-    //     //remove the 'New' badge on chat icon (if any) once chat is opened.
-    //     setTimeout(()=>{
-    //         if(document.querySelector('#chat-pane').classList.contains('chat-opened')){
-    //             helpers.toggleChatNotificationBadge();
-    //         }
-    //     }, 300);
-    // });
+        //remove the 'New' badge on chat icon (if any) once chat is opened.
+        setTimeout(()=>{
+            if(document.querySelector('#chat-pane').classList.contains('chat-opened')){
+                helpers.toggleChatNotificationBadge();
+            }
+        }, 300);
+    });
 
 
     //When the video frame is clicked. This will enable picture-in-picture
-    // document.getElementById('local-video').addEventListener('click', ()=>{
-    //     if (!document.pictureInPictureElement) {
-    //         document.getElementById('local-video').requestPictureInPicture()
-    //         .catch(error => {
-    //             // Video failed to enter Picture-in-Picture mode.
-    //             console.error(error);
-    //         });
-    //     } 
+    document.getElementById('local').addEventListener('click', ()=>{
+        if (!document.pictureInPictureElement) {
+            document.getElementById('local').requestPictureInPicture()
+            .catch(error => {
+                // Video failed to enter Picture-in-Picture mode.
+                console.error(error);
+            });
+        } 
           
-    //     else {
-    //         document.exitPictureInPicture()
-    //         .catch(error => {
-    //             // Video failed to leave Picture-in-Picture mode.
-    //             console.error(error);
-    //         });
-    //     }
-    // });
+        else {
+            document.exitPictureInPicture()
+            .catch(error => {
+                // Video failed to leave Picture-in-Picture mode.
+                console.error(error);
+            });
+        }
+    });
 
 
     //When the 'Create room" is button is clicked
-    document.getElementById('create-room').addEventListener('submit', (e)=>{
+    document.getElementById('create-room').addEventListener('click', (e)=>{
         e.preventDefault();
 
         let roomName = document.querySelector('#room-name').value;
         let yourName = document.querySelector('#your-name').value;
 
-        // let roomName = 'test';
-        // let yourName = 'test';
-
         if(roomName && yourName){
             //remove error message, if any
-            // document.querySelector('#err-msg').innerHTML = "";
+            document.querySelector('#err-msg').innerHTML = "";
 
             //save the user's name in sessionStorage
             sessionStorage.setItem('username', yourName);
 
             //create room link
-            let rommName = `${roomName.trim().replace(' ', '_')}_${helpers.generateRandomString()}`
-            let roomLink = `${location.origin}?room=${rommName}`;
+            let roomLink = `${location.origin}?room=${roomName.trim().replace(' ', '_')}_${helpers.generateRandomString()}`;
 
             //show message with link to room
             document.querySelector('#room-created').innerHTML = `Room successfully created. Click <a href='${roomLink}'>here</a> to enter room. 
                 Share the room link with your partners.`;
-            
-            // $("#room-created").load("create-meeting-act", {
-            //     host: yourName,
-            //     meeting_id: rommName
-            // });
 
             //empty the values
-            // document.querySelector('#room-name').value = '';
-            // document.querySelector('#your-name').value = '';
+            document.querySelector('#room-name').value = '';
+            document.querySelector('#your-name').value = '';
         }
 
         else{
@@ -76,25 +67,24 @@ window.addEventListener('load', ()=>{
 
 
     //When the 'Enter room' button is clicked.
-    // document.getElementById('enter-room').addEventListener('click', (e)=>{
-    //     e.preventDefault();
+    document.getElementById('enter-room').addEventListener('click', (e)=>{
+        e.preventDefault();
 
-    //     // let name = document.querySelector('#username').value;
-    //     let name = 'test';
+        let name = document.querySelector('#username').value;
 
-    //     if(name){
-    //         //remove error message, if any
-    //         // document.querySelector('#err-msg-username').innerHTML = "";
+        if(name){
+            //remove error message, if any
+            document.querySelector('#err-msg-username').innerHTML = "";
 
-    //         //save the user's name in sessionStorage
-    //         sessionStorage.setItem('username', name);
+            //save the user's name in sessionStorage
+            sessionStorage.setItem('username', name);
 
-    //         //reload room
-    //         location.reload();
-    //     }
+            //reload room
+            location.reload();
+        }
 
-    //     else{
-    //         document.querySelector('#err-msg-username').innerHTML = "Please input your name";
-    //     }
-    // });
+        else{
+            document.querySelector('#err-msg-username').innerHTML = "Please input your name";
+        }
+    });
 })
--- src/assets/js/rtc.js
+++ src/assets/js/rtc.js
@@ -2,282 +2,247 @@
  * @author Amir Sanni <amirsanni@gmail.com>
  * @date 6th January, 2020
  */
- import h from './helpers.js';
+import h from './helpers.js';
 
- window.addEventListener('load', ()=>{
-     const room = h.getQString(location.href, 'room');
-     const username = sessionStorage.getItem('username');
- 
-     if(!room){
-        window.location.href = "/create-meeting";
-     }
- 
-     else if(!username){
-        window.location.href = "/join-meeting?room=" + room;
-     }
- 
-     else{
-        //  let commElem = document.getElementsByClassName('room-comm');
- 
-        //  for(let i = 0; i < commElem.length; i++){
-        //      commElem[i].attributes.removeNamedItem('hidden');
-        //  }
- 
-         var pc = [];
- 
-         let socket = io('/stream');
- 
-         var socketId = '';
-         var myStream = '';
-         var strs = [];
-         var str = [];
- 
-         socket.on('connect', ()=>{
-             //set socketId
-            socketId = socket.io.engine.id+"____"+username;
-         
- 
-             socket.emit('subscribe', {
-                 room: room,
-                 socketId: socketId
-             });
- 
- 
-             socket.on('new user', (data)=>{
-                 socket.emit('newUserStart', {to:data.socketId, sender:socketId});
-                 pc.push(data.socketId);
-                 init(true, data.socketId);
-             });
- 
- 
-             socket.on('newUserStart', (data)=>{
-                 pc.push(data.sender);
-                 init(false, data.sender);
-             });
- 
- 
-             socket.on('ice candidates', async (data)=>{
-                 data.candidate ? await pc[data.sender].addIceCandidate(new RTCIceCandidate(data.candidate)) : '';
-             });
- 
- 
-             socket.on('sdp', async (data)=>{
-                 if(data.description.type === 'offer'){
-                     data.description ? await pc[data.sender].setRemoteDescription(new RTCSessionDescription(data.description)) : '';
- 
-                     h.getUserMedia().then(async (stream)=>{
-                         if(!document.getElementById('local-video').srcObject){
-                             document.getElementById('local-video').srcObject = stream;
-                         }
- 
-                         //save my stream
-                         myStream = stream;
- 
-                         stream.getTracks().forEach((track)=>{
-                             pc[data.sender].addTrack(track, stream);
-                         });
- 
-                         let answer = await pc[data.sender].createAnswer();
-                         
-                         await pc[data.sender].setLocalDescription(answer);
- 
-                         socket.emit('sdp', {description:pc[data.sender].localDescription, to:data.sender, sender:socketId});
-                     }).catch((e)=>{
-                         console.error(e);
-                     });
-                 }
- 
-                 else if(data.description.type === 'answer'){
-                     await pc[data.sender].setRemoteDescription(new RTCSessionDescription(data.description));
-                 }
-             });
- 
- 
-             socket.on('chat', (data)=>{
-                 h.addChat(data, 'remote');
-             })
-         });
- 
- 
-         function sendMsg(msg){
-             let data = {
-                 room: room,
-                 msg: msg,
-                 sender: username
-             };
- 
-             //emit chat message
-             socket.emit('chat', data);
- 
- 
-             //add localchat
-             h.addChat(data, 'local');
-         }
- 
- 
- 
-         function init(createOffer, partnerName){
-             pc[partnerName] = new RTCPeerConnection(h.getIceServer());
-             
-             h.getUserMedia().then((stream)=>{
-                 //save my stream
-                 myStream = stream;
- 
-                 stream.getTracks().forEach((track)=>{
-                     pc[partnerName].addTrack(track, stream);//should trigger negotiationneeded event
-                 });
- 
-                 document.getElementById('local-video').srcObject = stream;
-             }).catch((e)=>{
-                 console.error(`stream error: ${e}`);
-             });
- 
- 
- 
-             //create offer
-             if(createOffer){
-                 pc[partnerName].onnegotiationneeded = async ()=>{
-                     let offer = await pc[partnerName].createOffer();
-                     
-                     await pc[partnerName].setLocalDescription(offer);
- 
-                     socket.emit('sdp', {description:pc[partnerName].localDescription, to:partnerName, sender:socketId});
-                 };
-             }
- 
- 
- 
-             //send ice candidate to partnerNames
-             pc[partnerName].onicecandidate = ({candidate})=>{
-                 socket.emit('ice candidates', {candidate: candidate, to:partnerName, sender:socketId});
-             };
- 
- 
- 
-             //add
-             pc[partnerName].ontrack = (e)=>{
-                console.log("The user is connected");
-                let str = e.streams[0];
-                let partnerNameArr = partnerName.split('____');
-                let tempusername = partnerNameArr[partnerNameArr.length - 1];
-                if (document.getElementById(`${partnerName}-video`)) {
-                    // alert('if'+partnerName);
-                    document.getElementById(`${partnerName}-video`).srcObject = str;
-                    document.getElementById(`${partnerName}-p`).textContent = tempusername;
+window.addEventListener('load', ()=>{
+    const room = h.getQString(location.href, 'room');
+    const username = sessionStorage.getItem('username');
+
+    if(!room){
+        document.querySelector('#room-create').attributes.removeNamedItem('hidden');
+    }
+
+    else if(!username){
+        document.querySelector('#username-set').attributes.removeNamedItem('hidden');
+    }
+
+    else{
+        let commElem = document.getElementsByClassName('room-comm');
+
+        for(let i = 0; i < commElem.length; i++){
+            commElem[i].attributes.removeNamedItem('hidden');
+        }
+
+        var pc = [];
+
+        let socket = io('/stream');
+
+        var socketId = '';
+        var myStream = '';
+
+        socket.on('connect', ()=>{
+            //set socketId
+            socketId = socket.io.engine.id;
+        
+
+            socket.emit('subscribe', {
+                room: room,
+                socketId: socketId
+            });
+
+
+            socket.on('new user', (data)=>{
+                socket.emit('newUserStart', {to:data.socketId, sender:socketId});
+                pc.push(data.socketId);
+                init(true, data.socketId);
+            });
+
+
+            socket.on('newUserStart', (data)=>{
+                pc.push(data.sender);
+                init(false, data.sender);
+            });
+
+
+            socket.on('ice candidates', async (data)=>{
+                data.candidate ? await pc[data.sender].addIceCandidate(new RTCIceCandidate(data.candidate)) : '';
+            });
+
+
+            socket.on('sdp', async (data)=>{
+                if(data.description.type === 'offer'){
+                    data.description ? await pc[data.sender].setRemoteDescription(new RTCSessionDescription(data.description)) : '';
+
+                    h.getUserMedia().then(async (stream)=>{
+                        if(!document.getElementById('local').srcObject){
+                            document.getElementById('local').srcObject = stream;
+                        }
+
+                        //save my stream
+                        myStream = stream;
+
+                        stream.getTracks().forEach((track)=>{
+                            pc[data.sender].addTrack(track, stream);
+                        });
+
+                        let answer = await pc[data.sender].createAnswer();
+                        
+                        await pc[data.sender].setLocalDescription(answer);
+
+                        socket.emit('sdp', {description:pc[data.sender].localDescription, to:data.sender, sender:socketId});
+                    }).catch((e)=>{
+                        console.error(e);
+                    });
                 }
 
-                else {
-                    // alert('else'+partnerName);
+                else if(data.description.type === 'answer'){
+                    await pc[data.sender].setRemoteDescription(new RTCSessionDescription(data.description));
+                }
+            });
+
+
+            socket.on('chat', (data)=>{
+                h.addChat(data, 'remote');
+            })
+        });
+
+
+        function sendMsg(msg){
+            let data = {
+                room: room,
+                msg: msg,
+                sender: username
+            };
+
+            //emit chat message
+            socket.emit('chat', data);
+
+
+            //add localchat
+            h.addChat(data, 'local');
+        }
+
+
+
+        function init(createOffer, partnerName){
+            pc[partnerName] = new RTCPeerConnection(h.getIceServer());
+            
+            h.getUserMedia().then((stream)=>{
+                //save my stream
+                myStream = stream;
+
+                stream.getTracks().forEach((track)=>{
+                    pc[partnerName].addTrack(track, stream);//should trigger negotiationneeded event
+                });
+
+                document.getElementById('local').srcObject = stream;
+            }).catch((e)=>{
+                console.error(`stream error: ${e}`);
+            });
+
+
+
+            //create offer
+            if(createOffer){
+                pc[partnerName].onnegotiationneeded = async ()=>{
+                    let offer = await pc[partnerName].createOffer();
+                    
+                    await pc[partnerName].setLocalDescription(offer);
+
+                    socket.emit('sdp', {description:pc[partnerName].localDescription, to:partnerName, sender:socketId});
+                };
+            }
+
+
+
+            //send ice candidate to partnerNames
+            pc[partnerName].onicecandidate = ({candidate})=>{
+                socket.emit('ice candidates', {candidate: candidate, to:partnerName, sender:socketId});
+            };
+
+
+
+            //add
+            pc[partnerName].ontrack = (e)=>{
+                let str = e.streams[0];
+                if(document.getElementById(`${partnerName}-video`)){
+                    document.getElementById(`${partnerName}-video`).srcObject = str;
+                }
+
+                else{
                     //video elem
                     let newVid = document.createElement('video');
-                    newVid.id = `${partnerName}-video`;
+                    newVid.id = `${partnerName}-video`;            
                     newVid.srcObject = str;
                     newVid.autoplay = true;
-                    newVid.style.width = '100%';
-
+                    newVid.className = 'remote-video';
+                    
                     //create a new div for card
-                    // let cardDiv = document.createElement('div');
-                    // cardDiv.className = 'card mb-3';
-                    // cardDiv.appendChild(newVid);
-
+                    let cardDiv = document.createElement('div');
+                    cardDiv.className = 'card mb-3';
+                    cardDiv.appendChild(newVid);
+                    
                     //create a new div for everything
                     let div = document.createElement('div');
-                    div.className = 'videos';
+                    div.className = 'col-sm-12 col-md-6';
                     div.id = partnerName;
-                    div.style.position = 'relative';
-                    div.appendChild(newVid);
-
-                    let namediv = document.createElement('div');
-                    namediv.id = `${partnerName}-p`;
-                    namediv.style.padding = '2px 5px';
-                    namediv.style.borderRadius = '0px 0px 5px 5px';
-                    namediv.style.backgroundColor = 'white';
-                    namediv.style.fontSize = '10px';
-                    namediv.textContent = tempusername;
-                    div.appendChild(namediv);
-
-                    let adddiv = document.createElement('div');
-                    adddiv.className = 'add-div align-items-center justify-content-center';
-                    div.appendChild(adddiv);
-
-                    let addbutton = document.createElement('button');
-                    addbutton.id = partnerName+"-button";
-                    addbutton.addEventListener('click', function(){
-                        addtomain(str, tempusername)
-                    });
-                    addbutton.className = 'add-button';
-                    if(strs.includes(str)){
-                        addbutton.textContent = 'Remove from stream';
-                    } else{
-                        addbutton.textContent = 'Add to stream';
-                    }
-                    adddiv.appendChild(addbutton);
-
+                    div.appendChild(cardDiv);
+                    
                     //put div in videos elem
                     document.getElementById('videos').appendChild(div);
                 }
-             };
- 
- 
- 
-             pc[partnerName].onconnectionstatechange = (d)=>{
-                 switch(pc[partnerName].iceConnectionState){
-                     case 'disconnected':
-                     case 'failed':
-                         h.closeVideo(partnerName);
-                         break;
-                         
-                     case 'closed':
-                         h.closeVideo(partnerName);
-                         break;
-                 }
-             };
- 
- 
- 
-             pc[partnerName].onsignalingstatechange = (d)=>{
-                 switch(pc[partnerName].signalingState){
-                     case 'closed':
-                         console.log("Signalling state is 'closed'");
-                         h.closeVideo(partnerName);
-                         break;
-                 }
-             };
-         }
- 
- 
-        //  document.getElementById('chat-input').addEventListener('keypress', (e)=>{
-        //      if(e.which === 13 && (e.target.value.trim())){
-        //          e.preventDefault();
-                 
-        //          sendMsg(e.target.value);
- 
-        //          setTimeout(()=>{
-        //              e.target.value = '';
-        //          }, 50);
-        //      }
-        //  });
- 
- 
-        //  document.getElementById('toggle-video').addEventListener('click', (e)=>{
-        //      e.preventDefault();
- 
-        //      myStream.getVideoTracks()[0].enabled = !(myStream.getVideoTracks()[0].enabled);
- 
-        //      //toggle video icon
-        //      e.srcElement.classList.toggle('fa-video');
-        //      e.srcElement.classList.toggle('fa-video-slash');
-        //  });
- 
- 
-        //  document.getElementById('toggle-mute').addEventListener('click', (e)=>{
-        //      e.preventDefault();
- 
-        //      myStream.getAudioTracks()[0].enabled = !(myStream.getAudioTracks()[0].enabled);
- 
-        //      //toggle audio icon
-        //      e.srcElement.classList.toggle('fa-volume-up');
-        //      e.srcElement.classList.toggle('fa-volume-mute');
-        //  });
-     }
- });
+            };
+
+
+
+            pc[partnerName].onconnectionstatechange = (d)=>{
+                switch(pc[partnerName].iceConnectionState){
+                    case 'disconnected':
+                    case 'failed':
+                        h.closeVideo(partnerName);
+                        break;
+                        
+                    case 'closed':
+                        h.closeVideo(partnerName);
+                        break;
+                }
+            };
+
+
+
+            pc[partnerName].onsignalingstatechange = (d)=>{
+                switch(pc[partnerName].signalingState){
+                    case 'closed':
+                        console.log("Signalling state is 'closed'");
+                        h.closeVideo(partnerName);
+                        break;
+                }
+            };
+        }
+
+
+        document.getElementById('chat-input').addEventListener('keypress', (e)=>{
+            if(e.which === 13 && (e.target.value.trim())){
+                e.preventDefault();
+                
+                sendMsg(e.target.value);
+
+                setTimeout(()=>{
+                    e.target.value = '';
+                }, 50);
+            }
+        });
+
+
+        document.getElementById('toggle-video').addEventListener('click', (e)=>{
+            e.preventDefault();
+
+            myStream.getVideoTracks()[0].enabled = !(myStream.getVideoTracks()[0].enabled);
+
+            //toggle video icon
+            e.srcElement.classList.toggle('fa-video');
+            e.srcElement.classList.toggle('fa-video-slash');
+        });
+
+
+        document.getElementById('toggle-mute').addEventListener('click', (e)=>{
+            e.preventDefault();
+
+            myStream.getAudioTracks()[0].enabled = !(myStream.getAudioTracks()[0].enabled);
+
+            //toggle audio icon
+            e.srcElement.classList.toggle('fa-volume-up');
+            e.srcElement.classList.toggle('fa-volume-mute');
+        });
+    }
+});
